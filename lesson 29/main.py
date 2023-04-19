6829a9e1a975dc64952401fe743dc31bdd8465e9
movavi_school <grankina@nprofi.ru>
2023-04-19 20:57:02 +0700

    19 Апреля уже апрель это жесть

---
+++ lesson 29/main.py
@@ -0,0 +1,108 @@
+# from tkinter import *
+# # # # #
+# # # # # def printer(pipupa):
+# # # # #     print(cb_val.get())    # Booleanvar -> .get()
+# # # # #     #cb.deselect()    # Снять выделение
+# # # # #     #cb.select()  # Поставить выделение
+# # # # #
+# # # # # root = Tk()
+# # # # # root.geometry("250x370")
+# # # # #
+# # # # # cb_val = BooleanVar()    # Переменная отвечающая за хранение значения
+# # # # # cb = Checkbutton(root,
+# # # # #                  text="Подписка",
+# # # # #                  variable=cb_val,
+# # # # #                  onvalue=True,
+# # # # #                  offvalue=False)
+# # # # # cb.pack()
+# # # # # cb.bind("<Button-3>", printer)
+# # # # #
+# # # # #
+# # # # # root.mainloop()
+# # # # root = Tk()
+# # # #
+# # # #
+# # # # def get_rb():
+# # # #     print(rb_val.get())
+# # # #
+# # # #
+# # # #
+# # # #
+# # # # root.geometry("250x370")
+# # # #
+# # # # rb_val = IntVar()
+# # # # print(rb_val.get())
+# # # # Radiobutton(root, variable=rb_val, text="Один", value=505, command=get_rb).pack()
+# # # # Radiobutton(root, variable=rb_val, text="Два", value=47, command=get_rb).pack()
+# # # #
+# # # # root.mainloop()
+# # #
+# # #
+# # root = Tk()
+# # root.geometry("250x370")
+# # #
+# # # def pipu():
+# # #     print(lst.curselection())   # текущий выбор
+# # #
+# # # v = ["Пуджик", "Сфчик", "Течес"]
+# # #
+# # # lst = Listbox(root,
+# # #               selectmode=EXTENDED)
+# # # lst.pack()
+# # # for elem in v:
+# # #     lst.insert("end", elem)  # размещение элемента в Listbox
+# # #
+# # # Button(root, text="Активировать", command=pipu()).pack()
+# # #
+# # #
+# # #
+# # #
+# # # root.mainloop()
+# #
+# # def get_scale(e):
+# #     print(scal.get())
+# #     print(e)
+# #
+# # scal = Scale(root, from_=4, to=34,
+# #              orient=HORIZONTAL,# располож
+# #              length=200,  # Длинна
+# #              width=20,      # Ширина
+# #              tickinterval=2,
+# #              command=get_scale)  # Подписать с шагом
+# #
+# # scal.pack()
+# #
+# #
+# #
+# #
+# #
+# #
+# # root.mainloop()
+#
+# root = Tk()
+# root.geometry("500x300")
+#
+# def activatar():
+#     if btn1['state'] == "disabled": #Если кнопка выключена
+#         btn1['state'] = "normal"
+#     else:
+#         btn1['state'] = "disabled"
+#
+# btn1 = Button(root, text="ШлЁп")
+# btn1.pack()
+#
+# btn2 = Button(root, text="Процесс запуска боеголовки Альфа Активирован", command=activatar)
+# btn2.pack()
+#
+# root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson 29/task.py
@@ -0,0 +1,66 @@
+# from tkinter import *
+#
+# root = Tk()
+#
+# def bolt():
+#     if  val_bolt.get():
+#         lab['font'] += " bold"
+#     else:
+#         lab['font'] = lab['font'].replase(" bold", "")
+#
+#
+#
+# def zxc():
+#     if val_zxc.get():
+#         lab['font'] += " bold"
+#     else:
+#         lab['font'] = lab['font'].replase(" bold", "")
+#
+# def zas():
+#     if val
+#         lab['font'] += " bold"
+#     else:
+#         lab['font'] = lab['font'].replase(" bold", "")
+#
+#
+# def vmate():
+#
+# lab = Label(root, text="0 15 2 угу.", font="Arial 15")
+#
+# val_bolt = BooleanVar()
+# cb1 = Checkbutton(root,
+#                   text="Жижный",
+#                   command=bolt,
+#                   variable=val_bolt,
+#                   onvalue=True,
+#                   offvalue=False)
+#
+# val_zxc = BooleanVar()
+# cb2 = Checkbutton(root,
+#                   text="Курсед",
+#                   command=zxc,
+#                   variable=val_zxc,
+#                   onvalue=True,
+#                   offvalue=False)
+#
+#
+# val_zas = BooleanVar()
+# cb3 = Checkbutton(root,
+#                   text="Зачеченый",
+#                   command=zas,
+#                   variable=val_zas,
+#                   onvalue=True,
+#                   offvalue=False)
+#
+# val_vmate = BooleanVar()
+# cb4 = Checkbutton(root,
+#                   text="Жижный",
+#                   command=vmate,
+#                   variable=val_zas,
+#                   onvalue=True,
+#                   offvalue=False)
+#
+#
+#
+#
+# root.mainloop()
